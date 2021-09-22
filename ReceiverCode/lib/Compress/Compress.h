1c80b4e4d014a3a164d4528f6cbd0a1a07b53690
2bndy5 <2bndy5@gmail.com>
2021-09-21 22:42:38 -0700

    Revert "Library fixed (Temporarily) and pointers implemented"
    This reverts commit b8780be5d6f94b9669a882820536160994c376e6.

--- ReceiverCode/lib/Compress/Compress.h
+++ ReceiverCode/lib/Compress/Compress.h
@@ -1,14 +1,13 @@
 
-#ifndef Compress_h
-#define Compress_h
-#include "Arduino.h"
+#ifndef _RECEIVERCODE_LIB_COMPRESS_H
+#define _RECEIVERCODE_LIB_COMPRESS_H
+#include <stdint.h>
 
-class Compress                       
-{
-	public:
-		void DeComp(uint16_t *d, uint16_t *c,int cc);
-		void Comp(uint16_t *c, uint16_t *o, int cc);
-	private:
-  		
+class Compress {
+public:
+    void DeComp(uint16_t* d, uint16_t* c, int cc);
+    void Comp(uint16_t* c, uint16_t* o, int cc);
+
+private:
 };
 #endif
