c8502fb872263d1756be60af9469f3d8bbf4768b
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2021-09-26 10:43:50 +0100

    test update ... nothing meaningful

--- ReceiverCode/src/main.cpp
+++ ReceiverCode/src/main.cpp
@@ -7,7 +7,7 @@
 // #define DB_FAILSAFE
 // #define SECOND_TRANSCEIVER
 
-#define RECEIVE_TIMEOUT 66    // 15 milliseconds was too short  ........ updated?
+#define RECEIVE_TIMEOUT 66    // 15 milliseconds was too short 
 #define PacketsPerHop   20
 #define CHANNELSUSED    16
 #define SERVOSUSED      10
