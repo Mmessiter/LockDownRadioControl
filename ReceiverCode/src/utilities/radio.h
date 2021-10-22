f65bdd0865be0529e9b95292cac0d12dc03c2f6c
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2021-10-22 16:52:57 +0100

    Update radio.h

--- ReceiverCode/src/utilities/radio.h
+++ ReceiverCode/src/utilities/radio.h
@@ -160,7 +160,7 @@ void ProdRadio()
     CurrentRadio->setPALevel(RF24_PA_MAX);
     CurrentRadio->setDataRate(RF24_250KBPS);
     CurrentRadio->openReadingPipe(1, ThisPipe);
-    delay(35); // Delay requirement has NOT gone away! 
+    delay(50); // Delay requirement has NOT gone away! 
 }
 
 /************************************************************************************************************/
