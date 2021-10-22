2cce089dc7506164ed062e2e8b3f76b8da1c069d
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2021-10-22 13:39:16 +0100

    BINDPIPETIMEOUT increased from 10 to 50 ms

--- TransmitterCode/src/Hardware/RadioFunctions.cpp
+++ TransmitterCode/src/Hardware/RadioFunctions.cpp
@@ -20,7 +20,7 @@
 #define One_Switch_View    13
 #define Help_View          14
 #define Options_View       15
-#define BINDPIPETIMEOUT    10                        // timeout for switching from Bound to Default pipe
+#define BINDPIPETIMEOUT    50                        // timeout for switching from Bound to Default pipe ** TO LOW??? WAS 10 ... ***
 #define FHSS_RESCUE_BOTTOM 118                       // reduced range for recovery
 #define FHSS_RESCUE_TOP    125                       // reduced range for recovery
 #define UNCOMPRESSEDWORDS  20                        // DATA TO SEND = 40  Bytes
