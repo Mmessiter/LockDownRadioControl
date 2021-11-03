012a57c7f40888f1117855b2144426eeade12c69
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2021-11-03 11:47:08 +0000

    Update common.h

--- ReceiverCode/src/utilities/common.h
+++ ReceiverCode/src/utilities/common.h
@@ -13,7 +13,7 @@
 // #define DB_PID
 // #define DB_BIND
 // #define DB_FAILSAFE
-#define SECOND_TRANSCEIVER
+// #define SECOND_TRANSCEIVER
 // #define SECOND_TRANSCEIVER_DEBUG
 
 int  LastConnectionMoment = 0;
