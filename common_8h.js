6514ad279d6187fa9cd28cc508661571ee7387c2
Mmessiter <Mmessiter@users.noreply.github.com>
2021-11-12 17:18:04 +0000

    deploy: ae45559b3e53b9c5bf8c199f81a0b10f6562dca3

--- common_8h.js
+++ common_8h.js
@@ -1,6 +1,5 @@
 var common_8h =
 [
-    [ "DEBUG", "common_8h.html#ad72dbcf6d0153db1b8d8a58001feed83", null ],
     [ "NEW_FHSS", "common_8h.html#ac5f10e9a5bbd8efbba3eb8ec5061a8d9", null ],
     [ "RXVERSION_MAJOR", "common_8h.html#a2b147d5155fcf962e9f1c4885b32c67d", null ],
     [ "RXVERSION_MINIMUS", "common_8h.html#a46d5105ccf92f7e44b10af6fd51772a8", null ],
@@ -9,5 +8,6 @@ var common_8h =
     [ "LoadModelNumber", "common_8h.html#a235d91d2b354c3ce98152e3367ff0fb8", null ],
     [ "FailSafeSave", "common_8h.html#a332eaa61dd85cd649cf62de2345e246e", null ],
     [ "FailSafeSent", "common_8h.html#a97bbae82012a7c41f47dcb8ce8a48a90", null ],
+    [ "FHSS_Channels", "common_8h.html#a79ed13ac413de8a0a1767615afccbe2f", null ],
     [ "LastConnectionMoment", "common_8h.html#ae6d822db2ca02c3f6fa690311b580c93", null ]
 ];
