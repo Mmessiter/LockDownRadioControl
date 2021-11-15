aa2cb49254cad5843a279ff4ccb76df723828ce4
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2021-11-15 09:06:03 +0000

    fix

--- TransmitterCode/src/Hardware/RadioFunctions.cpp
+++ TransmitterCode/src/Hardware/RadioFunctions.cpp
@@ -28,7 +28,7 @@
 #define PACKETS_PER_HOP    20                        // Must match RX setting
 
 
-#ifndef NOISYWIFI // Use this array for UK legal flying 
+#ifndef NOISYWIFI // Use this for UK legal flying 
 #define RECONNECT_CH     86
 uint8_t FHSS_Channels[84] = {28,24,61,64,28,55,66,19,76,21,59,67,15,71,82,32,49,69,13,2,34,47,20,34,69,16,2,72,35,76,35,57,45,29,76,75,49,59,3,57,20,16,41,59,62,59,67,11,3,9,77,37,8,31,36,34,18,75,17,9,50,78,77,73,30,50,79,6,36,20,23,79,40,54,51,19,69,12,18,80,53,41,24};
 #endif
