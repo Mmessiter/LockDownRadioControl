899a5d8c439ec6741df15bf216e55678152e92cd
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2021-11-23 13:30:29 +0000

    Update main.cpp

--- TransmitterCode/src/main.cpp
+++ TransmitterCode/src/main.cpp
@@ -5551,11 +5551,12 @@ void GetRXTime(){  // This gets the time from Receivier to enable FHSS synch
 
 if ((TXTimeStamp == 0)|| (TXTimeStamp >= HOPTIME )) { // is it time (or indeed it is overdue?) to hop frequency?
         GetNextHopChannelNumber();       // Also gets actual channel number
-        NextFrequency=120; // temporary!!
-        HopToNextFrequency();
+        
         HopStart = millis()-2; 
         Serial.println(TXTimeStamp);
         Serial.println(FHSS_Channels[NextChannelNumber]);
+        NextFrequency=120; // temporary!!
+        HopToNextFrequency();
         TXTimeStamp = 0;
 }
       ClearAckPayload();
