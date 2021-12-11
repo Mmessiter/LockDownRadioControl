31fe8272416187b5aeb34fc87539cba6f6f07a87
Mmessiter <Mmessiter@users.noreply.github.com>
2021-12-11 11:08:41 +0000

    deploy: f1ac483c8d0639f3b6004abda44fe56a2b022b5f

--- search/defines_10.js
+++ search/defines_10.js
@@ -6,8 +6,8 @@ var searchData=
   ['red_1259',['Red',['../_transmitter_code_2src_2main_8cpp.html#ae8e246fd7335d7e27ab04150cbc418ba',1,'main.cpp']]],
   ['redled_1260',['REDLED',['../_transmitter_code_2src_2main_8cpp.html#a1a93f984896e07e32aff39c2e0fb4459',1,'main.cpp']]],
   ['renewdata_1261',['RENEWDATA',['../_transmitter_code_2src_2main_8cpp.html#abe07c12a93bec238e54292e20937dc11',1,'main.cpp']]],
-  ['retrycount_1262',['RetryCount',['../_radio_functions_8h.html#ad3e3c4d87628d85b85ce6773317642a7',1,'RadioFunctions.h']]],
-  ['retrywait_1263',['RetryWait',['../_radio_functions_8h.html#a2d8a411e5a5cb1b5ea4c5d39aae142c5',1,'RadioFunctions.h']]],
+  ['retrycount_1262',['RETRYCOUNT',['../_radio_functions_8h.html#a0cf4668725c06b3a6d64a2895c906379',1,'RadioFunctions.h']]],
+  ['retrywait_1263',['RETRYWAIT',['../_radio_functions_8h.html#aadc879b4b6396135d31e22c22955a753',1,'RadioFunctions.h']]],
   ['rxversion_5fmajor_1264',['RXVERSION_MAJOR',['../common_8h.html#a2b147d5155fcf962e9f1c4885b32c67d',1,'common.h']]],
   ['rxversion_5fminimus_1265',['RXVERSION_MINIMUS',['../common_8h.html#a46d5105ccf92f7e44b10af6fd51772a8',1,'common.h']]],
   ['rxversion_5fminor_1266',['RXVERSION_MINOR',['../common_8h.html#adeb76229aa4087c3bc1e1fd17bd865b2',1,'common.h']]]
