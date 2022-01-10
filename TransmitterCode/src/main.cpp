9b8706483f6d70af2461f9c209e1f10b5ba8a402
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-01-10 09:21:27 +0000

    Update main.cpp

--- TransmitterCode/src/main.cpp
+++ TransmitterCode/src/main.cpp
@@ -492,8 +492,6 @@ uint16_t Qnh            = 1009;               // pressure at sea level here
 uint16_t ModelNumberOffset = 0;
 uint32_t ModelNameTimeCheck = 0;
 uint16_t LastModelLoaded    = 0;
-
-
 uint8_t FHSS_Channels[84] = {28, 24, 61, 64, 28, 55, 66, 19, 76, 21, 59, 67, 15, 71, 82, 32, 49, 69, 13, 2, 34, 47, 20,
                              34, 69, 16, 2, 72, 35, 76, 35, 57, 45, 29, 76, 75, 49, 59, 3, 57, 20, 16, 41, 59, 62, 59,
                              67, 11, 3, 9, 77, 37, 8, 31, 36, 34, 18, 75, 17, 9, 50, 78, 77, 73, 30, 50, 79, 6, 36,
