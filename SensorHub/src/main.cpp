13369f7ccf3386828d30fccd82d6345863b99b92
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-01-18 21:59:28 +0000

    update

--- SensorHub/src/main.cpp
+++ SensorHub/src/main.cpp
@@ -6,10 +6,15 @@ void requestEvent() {
 }
 
 void loop() {
-  delay(100);
+  char a;
+  while (Serial1.available()){
+    a = Serial1.read();
+    Serial.print (a);
+  }
 }
 
 void setup() {
+  Serial1.begin(9600);
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
 }
