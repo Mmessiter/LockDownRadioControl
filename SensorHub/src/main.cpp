24a0d839b50752d0c993a98fbabbfe41dfaf44a8
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-01-19 09:16:11 +0000

    Update main.cpp

--- SensorHub/src/main.cpp
+++ SensorHub/src/main.cpp
@@ -1,20 +1,42 @@
+
+//***********************************************************************************************************
+//************************************* SENSOR HUB CODE *****************************************************
+//***********************************************************************************************************
 #include <Arduino.h>
 #include <Wire.h>
+#define I2CADDRESS 8
+#define GPSBAUDRATE 9600
+//************************************* SEND DATA INTERRUPT HANDLER ******************************************
 
-void requestEvent() {
-  Wire.write("Hi There! "); 
+void SendEvent() {
+      Wire.write("Hi There! "); 
 }
+//************************************* RECEIVE DATA INTERRUPT HANDLER ***************************************
 
-void loop() {
-  char a;
-  while (Serial1.available()){
+void ReceiveEvent(int q) {
+  while(Wire.available()) 
+  {
+    char c = Wire.read(); 
+    Serial.print(c);        
+  }      
+   Serial.println("");    
+}
+//*************************************** READ GPS DEVICE ***************************************************
+ void ReadGps() {
+    char a;
+    while (Serial1.available()){
     a = Serial1.read();
     Serial.print (a);
-  }
+   }
+ }
+//*************************************** MAIN LOOP **********************************************************
+void loop() {
+    ReadGps();
 }
-
+//**************************************** SETUP *************************************************************
 void setup() {
-  Serial1.begin(9600);
-  Wire.begin(8);                // join i2c bus with address #8
-  Wire.onRequest(requestEvent); // register event
+  Serial1.begin(GPSBAUDRATE);
+  Wire.begin(I2CADDRESS);               
+  Wire.onRequest(SendEvent);    
+  Wire.onReceive(ReceiveEvent);
 }
