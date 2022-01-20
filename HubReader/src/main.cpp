138cc1c686299b9c45277aed8136364f77d6b22c
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-01-20 21:35:19 +0000

    update

--- HubReader/src/main.cpp
+++ HubReader/src/main.cpp
@@ -2,25 +2,34 @@
 #include <Wire.h>
 int led = LED_BUILTIN;
 
-void loop()
-{
+
+
+void GetI2CData(){
   Serial.print("Received over I2C: ");
-
-  digitalWrite(led, HIGH);  // briefly flash the LED
-  Wire.requestFrom(8, 9);   // request 6 bytes from slave device #8
-
-  while(Wire.available()) { // slave may send less than requested
-    char c = Wire.read();   // receive a byte as character
-    Serial.print(c);        // print the character
+  Wire.requestFrom(8, 9);  
+  while(Wire.available()) { 
+    char c = Wire.read();  
+    Serial.print(c);   
   }
   Serial.println();
-  digitalWrite(led, LOW);
-  delay(500);
 }
 
+void SendI2CData(){
+
+  Wire.beginTransmission(8);   
+  Wire.write("This is a test message ...  ");
+  Wire.endTransmission();   
+
+}
+
+void loop()
+{
+  SendI2CData();
+  delay(250);
+  GetI2CData();
+
+}
 void setup()
 {
-  pinMode(led, OUTPUT);
-  Wire.begin();             // join i2c bus (address optional for master)
-  Serial.begin(9600);       // start serial for output
+  Wire.begin();  
 }
