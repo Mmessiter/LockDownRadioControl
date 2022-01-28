e537c5b960206ce7f6b59cc1663fcb801b7fde36
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-01-28 17:56:48 +0000

    Update main.cpp

--- ReceiverCode/src/main.cpp
+++ ReceiverCode/src/main.cpp
@@ -522,11 +522,10 @@ FASTRUN void ReceiveData()
 { 
     if (millis() - LastConnectionMoment < 1 ) { // If, and only if, we have still absolutely loads of time, read the sensors now while waiting for next data packet.
        ReadSensors();                           // This must return in less than < 7 ms when next packet is due.
-       if ((millis() - LastConnectionMoment) > 1) Serial.println (millis() - LastConnectionMoment);   // (we can current expect to lose about 2 - 4 ms here if all are connected.)
+      // if ((millis() - LastConnectionMoment) > 1) Serial.println (millis() - LastConnectionMoment);   // We can expect to lose about 2 - 4 ms here if all are connected. 
+                                                                                                        // It MUST not exceed 6 ms!
     }
-
     Connected = false;                          // Assume not connected until we know better  
-    
     if (CurrentRadio->available()) {
         Connected = true;                        // ... ok we now know better ! 
     }
