cd6a5140074f57b8bbd3a1cc13e041a53c8520a2
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-02-01 11:01:11 +0000

    update

--- SensorHubLC/src/main.cpp
+++ SensorHubLC/src/main.cpp
@@ -1,7 +1,7 @@
 //***********************************************************************************************************
 //************************************* SENSOR HUB CODE FOR TEENSY LC  *************************************
 // 
-//                                   Version 1.1 Jan 29th 2022
+//                                   Version 1.2 Jan 31st 2022
 //                                   Uses Wire2 in Slave Mode ...
 //***********************************************************************************************************
 #include <Arduino.h>
@@ -31,8 +31,8 @@ uint8_t GPSYear;
 uint8_t GPSFix;
 const   char *  GPSLibVersion[20];
 float   GPSCourse;
-double  GPSDistanceTo;
-double  GPSCourseTo; 
+float   GPSDistanceTo;
+float   GPSCourseTo; 
 static  const double MAYSLANE_LAT = 51.638963994850364;       // Mays Lane location
 static  const double MAYSLANE_LON = -0.22926821753992477;     // Mays Lane location
 float   DestinationLat = MAYSLANE_LAT;
@@ -44,24 +44,21 @@ char    PMTK_SET_NMEA_OUTPUT_RMCGGAGSA[] =  "$PMTK314,0,1,0,1,1,0,0,0,0,0,0,0,0,
 char    PMTK_SET_BAUD_9600[]             =  "$PMTK251,9600*17";     // <   9600 bps     
 char    PGCMD_NOANTENNA[]                =  "$PGCMD,33,0*6D" ;      // < don't show antenna status messages
 uint8_t ParameterNumber                  = 0;
-bool    USE_BMP280 = false;                   //  BMP280 sensor connected ?
-bool    USE_INA219 = false;                   //  Volts from INA219 ?
+bool    FOUND_BMP280 = false;                   //  BMP280 sensor connected ?
+bool    FOUND_INA219 = false;                   //  Volts from INA219 ?
 Adafruit_INA219 ina219;
 Adafruit_BMP280 bmp280;
 uint32_t LoopTimer;
-
 float    BaroTemperature ;
 float    BaroAltitude ;
 float    INA219Volts;
-uint16_t Qnh = 1033;
-
-
+uint16_t Qnh = 0;
 
 //************************************* SEND DATA INTERRUPT HANDLER ******************************************
-// 
+ 
 void SendDataToReceiver() {
   #define IDLEN 3
-  #define GPSI2CBYTES IDLEN + 8
+  #define GPSI2CBYTES IDLEN + 4
   #define MAXPARAMS 14
   uint8_t i;
   char RdataID[IDLEN+1] = "NUL";
@@ -81,12 +78,11 @@ void SendDataToReceiver() {
   char  TMP[IDLEN+1]    = "TMP";
   char  VLT[IDLEN+1]    = "VLT";
 
-
-  double RdataOut = 42;
-  union { double   Val64;uint8_t Val8[8]; } Rdata;
+  float RdataOut = 42;
+  union { float   Val32;uint8_t Val8[4]; } Rdata;
   switch (ParameterNumber) {
       case  0:
-        RdataOut = GPSLatitude;     // Latitiude
+        RdataOut =  GPSLatitude;    // Latitiude
         strcpy (RdataID,LAT);
         break;
       case  1:
@@ -94,11 +90,11 @@ void SendDataToReceiver() {
         strcpy (RdataID,LON);
         break;
       case  2:
-        RdataOut = GPSFix;          // Fix
+        RdataOut = (float) GPSFix;  // Fix
         strcpy (RdataID,FIX);
         break;
       case  3:
-        RdataOut = GPSAltitude;     // Altitude
+        RdataOut =  GPSAltitude;    // Altitude
         strcpy (RdataID,ALT);
         break;
       case  4:
@@ -110,54 +106,51 @@ void SendDataToReceiver() {
         strcpy (RdataID,COR);
         break;
       case  6:
-        RdataOut = GPSCourseTo;     // Course to
+        RdataOut =  GPSCourseTo;     // Course to
         strcpy (RdataID,CTO);
         break;
       case  7:
-        RdataOut = GPSDistanceTo;   // Distance to
+        RdataOut =  GPSDistanceTo;   // Distance to
         strcpy (RdataID,DTO);
         break;
       case  8:
-        RdataOut = GPSHours;        // Hours
+        RdataOut = (float) GPSHours;  // Hours
         strcpy (RdataID,HRS);
         break;
       case  9:
-        RdataOut = GPSMins;        // Mins
+        RdataOut = (float) GPSMins;        // Mins
         strcpy (RdataID,MNS);
         break;
       case  10:
-        RdataOut = GPSSecs;        // Secs
+        RdataOut = (float) GPSSecs;        // Secs
         strcpy (RdataID,SEC);
         break;
       case  11:
-        RdataOut = GPSSatellites;  // Sats
+        RdataOut =  (float) GPSSatellites;  // Sats
         strcpy (RdataID,SAT);
         break;
       case  12:
-        RdataOut = BaroAltitude;  // ALT FROM BMP280
+        RdataOut = BaroAltitude;            // ALT FROM BMP280
         strcpy (RdataID,BLT);
         break;
       case  13:
-        RdataOut = BaroTemperature;  // TEMPERATURE FROM BMP280
+        RdataOut =  BaroTemperature;        // TEMPERATURE FROM BMP280
         strcpy (RdataID,TMP);
         break;
       case  14:
-        RdataOut = INA219Volts;     // VOLTAGE FROM INA219
+        RdataOut =  INA219Volts;     // VOLTAGE FROM INA219
         strcpy (RdataID,VLT);
         break;
       default:
         break;
-    
   }
-   Rdata.Val64 = RdataOut;
-   
+   Rdata.Val32 = RdataOut;
    for (i = 0; i < IDLEN; ++i) {
       Wire1.write(RdataID[i]); 
    }
-    for (i = 0; i < 8; ++i) {
+    for (i = 0; i < 4; ++i) {
        Wire1.write(Rdata.Val8[i]); 
     }
-
     ParameterNumber++;
     if (ParameterNumber > MAXPARAMS) ParameterNumber = 0; 
 }
@@ -166,9 +159,11 @@ void SendDataToReceiver() {
 void ReceiveEvent(int q) {  
 char MRK[] = "MRK";
 char MAY[] = "MAY";
-char RCV[4];
+char QNH[] = "QNH";
+char RCV[10];
+ union {uint16_t Val16; uint8_t Val8[2];} Uqnh;  
 
-    for (uint8_t i = 0; i < 3; ++i) {
+    for (uint8_t i = 0; i < 6; ++i) {
         if (Wire1.available()) {
             RCV[i]= Wire1.read();    // Get one byte at a time
         }
@@ -182,37 +177,43 @@ char RCV[4];
   }
 
  if (strcmp(MAY,RCV) == 0) {      // Match first 3 chars with MAY?
-     DestinationLat = MAYSLANE_LAT;  // Mark MAYS LANE location
-     DestinationLng = MAYSLANE_LON;  // Mark MAYS LANE location
-     return;
+      DestinationLat = MAYSLANE_LAT;  // Mark MAYS LANE location
+      DestinationLng = MAYSLANE_LON;  // Mark MAYS LANE location
+      return;
+  }
+
+  if (strcmp(QNH,RCV) == 0) {      // Match first 3 chars with QNH?
+      Uqnh.Val8[0] = RCV[4];       // 16 bit value for "Qnh" sent in bytes 4 and 5
+      Uqnh.Val8[1] = RCV[5]; 
+      Qnh = Uqnh.Val16;
+      return;
   }
 
 }
 //*************************************** READ GPS DEVICE ***************************************************
  void ReadGps() {
-
    char a;
     while (GPSDEVICE.available()){
       a = GPSDEVICE.read();
-      gps.encode(a);  // Simply send every byte library 
+      gps.encode(a);  // Simply send every byte to the library 
    }
-      GPSFix         = gps.location.isValid();
+      GPSFix           = gps.location.isValid();
       if (GPSFix) {
-        GPSLatitude    = gps.location.lat();
-        GPSLongitude   = gps.location.lng();
-        GPSSatellites  = gps.satellites.value(); 
-        GPSAltitude    = gps.altitude.feet();
-        GPSSpeed       = gps.speed.mph();
-        GPSHours       = gps.time.hour();   
-        GPSMins        = gps.time.minute(); 
-        GPSSecs        = gps.time.second(); 
-        GPSDay         = gps.date.day();
-        GPSMonth       = gps.date.month();
-        GPSYear        = gps.date.year();
+        GPSLatitude    = (float) gps.location.lat();
+        GPSLongitude   = (float) gps.location.lng();
+        GPSSatellites  = (uint8_t) gps.satellites.value(); 
+        GPSAltitude    = (float) gps.altitude.feet();
+        GPSSpeed       = (float) gps.speed.mph();
+        GPSHours       = (uint8_t) gps.time.hour();   
+        GPSMins        = (uint8_t) gps.time.minute(); 
+        GPSSecs        = (uint8_t) gps.time.second(); 
+        GPSDay         = (uint8_t) gps.date.day();
+        GPSMonth       = (uint8_t) gps.date.month();
+        GPSYear        = (uint8_t) gps.date.year();
         *GPSLibVersion = gps.libraryVersion();
-        GPSCourse      = gps.course.deg();
-        GPSDistanceTo  = gps.distanceBetween(GPSLatitude,GPSLongitude,DestinationLat,DestinationLng);
-        GPSCourseTo    = gps.courseTo(GPSLatitude,GPSLongitude,DestinationLat,DestinationLng);
+        GPSCourse      = (float) gps.course.deg();
+        GPSDistanceTo  = (float) gps.distanceBetween(GPSLatitude,GPSLongitude,DestinationLat,DestinationLng);
+        GPSCourseTo    = (float) gps.courseTo(GPSLatitude,GPSLongitude,DestinationLat,DestinationLng);
       }
  }
 // *********************************************** DEBUG DATA ***********************************************
@@ -262,11 +263,11 @@ void ShowGPS(){
 }
 // ***********************************************************************************************************
 void ReadOtherSensors(){
-    if (USE_BMP280) {
+    if (FOUND_BMP280) {
         BaroTemperature = bmp280.readTemperature();
-        BaroAltitude    = bmp280.readAltitude(Qnh) * 3.28084; 
+        BaroAltitude    = (bmp280.readAltitude(Qnh)) * 3.28084; 
     }
-    if (USE_INA219) INA219Volts = ina219.getBusVoltage_V();
+    if (FOUND_INA219) INA219Volts = ina219.getBusVoltage_V();
 }
 //*************************************** SendToGPS  **********************************************************
 void SendToGPS(char Cmd[80]){
@@ -310,13 +311,13 @@ void ScanI2c()
         delay(10);
         if (Wire.endTransmission() == 0) {
              if (i == 0x40) {
-                USE_INA219 = true;
+                FOUND_INA219 = true;
 #ifdef DEBUG
                 Serial.println("INA219 voltage meter detected!");
 #endif
               }
              if (i == 0x76) {
-                USE_BMP280 = true;
+                FOUND_BMP280 = true;
 #ifdef DEBUG
                 Serial.println("BMP280 barometer detected!");
 #endif
@@ -326,7 +327,6 @@ void ScanI2c()
         }
     }
 }
-
 //**************************************** SETUP *************************************************************
 void setup() {
   Wire1.begin(I2CADDRESS);                        // Wire1 MUST boot up BEFORE the receiver in order to be found by it.
@@ -335,8 +335,8 @@ void setup() {
   GPSDEVICE.begin(GPSBAUDRATE);
   Wire.begin();                                   // Wire used to read locally attached i2c sensors
   ScanI2c();
-  if (USE_BMP280) InitBMP280();
-  if (USE_INA219) ina219.begin();
+  if (FOUND_BMP280) InitBMP280();
+  if (FOUND_INA219) ina219.begin();
   delay (100);
   SendToGPS(PGCMD_NOANTENNA);                     // These setup commands are for Adafruit Ulimate GPS only
   delay (100);
