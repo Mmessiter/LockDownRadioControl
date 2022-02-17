09cb2c350fe0a337fe1a93091bfea943ac4b6182
Mmessiter <Mmessiter@users.noreply.github.com>
2022-02-17 11:19:38 +0000

    deploy: e52ca79a59d43ac6c97111499d0b8fe861cf18ca

--- search/all_15.js
+++ search/all_15.js
@@ -1,17 +1,18 @@
 var searchData=
 [
   ['ukrules_0',['UkRules',['../_transmitter_code_2src_2main_8cpp.html#a7847cc790a939f378993691ae7d40a2f',1,'main.cpp']]],
-  ['um_1',['um',['../_transmitter_code_2src_2main_8cpp.html#a9f83d3bf4d2c0b217e0e461d0c2f3e71',1,'main.cpp']]],
-  ['uncompressedwords_2',['UNCOMPRESSEDWORDS',['../radio_8h.html#a0c40a351a0c0e01fbd5e5b1b0187eb48',1,'UNCOMPRESSEDWORDS():&#160;radio.h'],['../_radio_functions_8cpp.html#a0c40a351a0c0e01fbd5e5b1b0187eb48',1,'UNCOMPRESSEDWORDS():&#160;RadioFunctions.cpp'],['../_transmitter_code_2src_2main_8cpp.html#a0c40a351a0c0e01fbd5e5b1b0187eb48',1,'UNCOMPRESSEDWORDS():&#160;main.cpp']]],
-  ['unit_3',['unit',['../structnmea__datavalue__t.html#a904af9df777122471b3358c1af5e5aac',1,'nmea_datavalue_t']]],
-  ['updatebuttonlabels_4',['UpdateButtonLabels',['../_transmitter_code_2src_2main_8cpp.html#abf59862cf6c4e3df5a76f0bfea8919ef',1,'main.cpp']]],
-  ['updateinterpolationtypes_5',['updateInterpolationTypes',['../_transmitter_code_2src_2main_8cpp.html#ab07d91dcd72f515d2d80b1d18f95ece5',1,'main.cpp']]],
-  ['updatemodelsnameeverywhere_6',['UpdateModelsNameEveryWhere',['../_transmitter_code_2src_2main_8cpp.html#a9e1de754e1d2d483fb15957f70cf0488',1,'main.cpp']]],
-  ['updateoneswitchview_7',['updateOneSwitchView',['../_transmitter_code_2src_2main_8cpp.html#a14dcac76ed8a048041fdc6e6f5d5d000',1,'main.cpp']]],
-  ['updateswitchesdisplay_8',['UpdateSwitchesDisplay',['../_transmitter_code_2src_2main_8cpp.html#a789078ba3c1d92997ab8764eb20cce4f',1,'main.cpp']]],
-  ['updatetrimview_9',['UpdateTrimView',['../_transmitter_code_2src_2main_8cpp.html#ac78966debc2cdc13d8ca958ce3b20c78',1,'main.cpp']]],
-  ['use_5fina219_10',['USE_INA219',['../_transmitter_code_2src_2main_8cpp.html#a87ae50e2ac4507568cbf98072f174a1b',1,'main.cpp']]],
-  ['use_5fsw_5fserial_11',['USE_SW_SERIAL',['../_adafruit___g_p_s_8h.html#a7d44b2886944eb862b51948f2acc8d23',1,'Adafruit_GPS.h']]],
-  ['use_5fwatchdog_12',['USE_WATCHDOG',['../_radio_functions_8h.html#a71be4a990011c7b00830abf5f3b2190d',1,'RadioFunctions.h']]],
-  ['usereceiveddata_13',['UseReceivedData',['../_receiver_code_2src_2main_8cpp.html#abb1997c589abc6432f612eaedf985e38',1,'main.cpp']]]
+  ['ukrulescounter_1',['UkRulesCounter',['../_transmitter_code_2src_2main_8cpp.html#a29883074027da47422a24b04284728db',1,'main.cpp']]],
+  ['um_2',['um',['../_transmitter_code_2src_2main_8cpp.html#a9f83d3bf4d2c0b217e0e461d0c2f3e71',1,'main.cpp']]],
+  ['uncompressedwords_3',['UNCOMPRESSEDWORDS',['../radio_8h.html#a0c40a351a0c0e01fbd5e5b1b0187eb48',1,'UNCOMPRESSEDWORDS():&#160;radio.h'],['../_radio_functions_8cpp.html#a0c40a351a0c0e01fbd5e5b1b0187eb48',1,'UNCOMPRESSEDWORDS():&#160;RadioFunctions.cpp'],['../_transmitter_code_2src_2main_8cpp.html#a0c40a351a0c0e01fbd5e5b1b0187eb48',1,'UNCOMPRESSEDWORDS():&#160;main.cpp']]],
+  ['unit_4',['unit',['../structnmea__datavalue__t.html#a904af9df777122471b3358c1af5e5aac',1,'nmea_datavalue_t']]],
+  ['updatebuttonlabels_5',['UpdateButtonLabels',['../_transmitter_code_2src_2main_8cpp.html#abf59862cf6c4e3df5a76f0bfea8919ef',1,'main.cpp']]],
+  ['updateinterpolationtypes_6',['updateInterpolationTypes',['../_transmitter_code_2src_2main_8cpp.html#ab07d91dcd72f515d2d80b1d18f95ece5',1,'main.cpp']]],
+  ['updatemodelsnameeverywhere_7',['UpdateModelsNameEveryWhere',['../_transmitter_code_2src_2main_8cpp.html#a9e1de754e1d2d483fb15957f70cf0488',1,'main.cpp']]],
+  ['updateoneswitchview_8',['updateOneSwitchView',['../_transmitter_code_2src_2main_8cpp.html#a14dcac76ed8a048041fdc6e6f5d5d000',1,'main.cpp']]],
+  ['updateswitchesdisplay_9',['UpdateSwitchesDisplay',['../_transmitter_code_2src_2main_8cpp.html#a789078ba3c1d92997ab8764eb20cce4f',1,'main.cpp']]],
+  ['updatetrimview_10',['UpdateTrimView',['../_transmitter_code_2src_2main_8cpp.html#ac78966debc2cdc13d8ca958ce3b20c78',1,'main.cpp']]],
+  ['use_5fina219_11',['USE_INA219',['../_transmitter_code_2src_2main_8cpp.html#a87ae50e2ac4507568cbf98072f174a1b',1,'main.cpp']]],
+  ['use_5fsw_5fserial_12',['USE_SW_SERIAL',['../_adafruit___g_p_s_8h.html#a7d44b2886944eb862b51948f2acc8d23',1,'Adafruit_GPS.h']]],
+  ['use_5fwatchdog_13',['USE_WATCHDOG',['../_radio_functions_8h.html#a71be4a990011c7b00830abf5f3b2190d',1,'RadioFunctions.h']]],
+  ['usereceiveddata_14',['UseReceivedData',['../_receiver_code_2src_2main_8cpp.html#abb1997c589abc6432f612eaedf985e38',1,'main.cpp']]]
 ];
