d3dde23c2caead38d80e2d55c506bb88837996a0
Mmessiter <Mmessiter@users.noreply.github.com>
2022-02-21 11:09:22 +0000

    deploy: c2b8cdefece45f0b51fbd4c60e773956e92fcadd

--- navtreedata.js
+++ navtreedata.js
@@ -155,8 +155,8 @@ var NAVTREEINDEX =
 "_transmitter_code_2src_2main_8cpp.html#afb18d0459efaad441f7c0cf7a7faec84",
 "class_adafruit___b_m_p280.html#ab28e689ef7620d2b9f0c9e03116dde8fa544ab88771584cea12877d3338eef96d",
 "class_adafruit___s_p_i_device.html#a5c4fce15c92a0210c0872a827f239ab1",
-"globals_defs_y.html",
-"struct_tiny_g_p_s_date.html#a2bcf8d5290fbef8c44b415e281b8c6f2"
+"globals_defs_x.html",
+"struct_tiny_g_p_s_date.html#a1b913837130bb49ecd309eaf6bc205b1"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
