65ccd05646fc7c1b84ae89388fe69fd28369de51
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-02-24 22:27:18 +0000

    Update main.cpp

--- TransmitterCode/src/main.cpp
+++ TransmitterCode/src/main.cpp
@@ -2835,9 +2835,8 @@ void setup()
     SendValue(FrontView_Hours, 0);
     SendValue(FrontView_Mins, 0);
     SendValue(FrontView_Secs, 0);
-
     //  ***************************************************************************************
-     // SetDS1307ToCompilerTime();    //  **   Uncomment this line to set DS1307 clock to compiler's (Computer's) time.        **
+    // SetDS1307ToCompilerTime();    //  **   Uncomment this line to set DS1307 clock to compiler's (Computer's) time.        **
     //  **   BUT then re-comment it!! Otherwise it will reset to same time on every boot up! **
     //  ***************************************************************************************
     RecoveryTimer = millis();
