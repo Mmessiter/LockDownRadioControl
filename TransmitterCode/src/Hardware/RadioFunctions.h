ab4bc0be6f2c125b007f9dd470ab0456fe18e78e
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-03-13 20:26:17 +0000

    update

--- TransmitterCode/src/Hardware/RadioFunctions.h
+++ TransmitterCode/src/Hardware/RadioFunctions.h
@@ -3,7 +3,7 @@
 #define RadioFunctions_H
 
 // **************************************************************************
-//                TX VERSION NUMBER   (March 11th 2022 Malcolm Messiter) *
+//                TX VERSION NUMBER   (March 13th 2022 Malcolm Messiter) *
 //***************************************************************************
 #define TXVERSION_MAJOR   1
 #define TXVERSION_MINOR   6
