9f0012f07851787e99f23638a76aa40af0af1450
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-05-05 21:37:32 +0100

    Update common.h

--- ReceiverCode/src/utilities/common.h
+++ ReceiverCode/src/utilities/common.h
@@ -15,7 +15,7 @@
     
     #define RXVERSION_MAJOR             1
     #define RXVERSION_MINOR             7
-    #define RXVERSION_MINIMUS           0    // April 18th 2022
+    #define RXVERSION_MINIMUS           1   // May 5th 2022
 
 //**************************************************************************************************************************
     #define SECOND_TRANSCEIVER // >>>>>>>>>>>>>>>> ******* DON'T FORGET TO SET THIS ONE !!! ******* <<<<<<<<<<<<<<<<<<<<< **
