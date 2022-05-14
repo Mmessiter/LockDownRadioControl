60e77f2c2c163bb29e7e226d6f11e7d32071eec7
Mmessiter <66127058+Mmessiter@users.noreply.github.com>
2022-05-14 16:39:55 +0100

    Update RadioFunctions.h

--- TransmitterCode/src/Hardware/RadioFunctions.h
+++ TransmitterCode/src/Hardware/RadioFunctions.h
@@ -3,11 +3,11 @@
 #define RadioFunctions_H
 
 // **************************************************************************
-//                TX VERSION NUMBER   (May 12th 2022 Malcolm Messiter) *
+//                TX VERSION NUMBER   (May 14th 2022 Malcolm Messiter) *
 //***************************************************************************
 #define TXVERSION_MAJOR   1
 #define TXVERSION_MINOR   7
-#define TXVERSION_MINIMUS 3
+#define TXVERSION_MINIMUS 4
 
 
 // **************************************************************************
@@ -35,7 +35,7 @@
 //                            FHSS PARAMETERS                               *
 //***************************************************************************
 
-#define PACEMAKER                   7   // MINIMUM ms between sent packets of data. These brief pauses allow the receiver to poll its i2c Sensor hub, and TX to ShowComms();
+#define PACEMAKER                   8   // MINIMUM ms between sent packets of data. These brief pauses allow the receiver to poll its i2c Sensor hub, and TX to ShowComms();
 #define RETRYCOUNT                  3   // auto retries from nRF24L01
 #define RETRYWAIT                   1   // Wait between retries is RetryWait+1 * 250us. A failed packet therefore takes (RetryWait+1 * 250us) * RetryCount
 #define LOSTCONTACTCUTOFF           6   // How many packets to lose before reconnect triggers  (>6)
