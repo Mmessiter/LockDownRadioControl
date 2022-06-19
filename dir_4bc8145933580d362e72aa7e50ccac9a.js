4f149afd0a6c3200043c9fecd98018e4bfe31240
Mmessiter <Mmessiter@users.noreply.github.com>
2022-06-19 17:20:11 +0000

    deploy: d1a085ca2e515dd5a9dc2e2c00eb9059454a4e8a

--- dir_4bc8145933580d362e72aa7e50ccac9a.js
+++ dir_4bc8145933580d362e72aa7e50ccac9a.js
@@ -3,9 +3,7 @@ var dir_4bc8145933580d362e72aa7e50ccac9a =
     [ "Adafruit_BusIO_Register.cpp", "_adafruit___bus_i_o___register_8cpp.html", null ],
     [ "Adafruit_BusIO_Register.h", "_adafruit___bus_i_o___register_8h.html", "_adafruit___bus_i_o___register_8h" ],
     [ "Adafruit_I2CDevice.cpp", "_adafruit___i2_c_device_8cpp.html", null ],
-    [ "Adafruit_I2CDevice.h", "_adafruit___i2_c_device_8h.html", [
-      [ "Adafruit_I2CDevice", "class_adafruit___i2_c_device.html", "class_adafruit___i2_c_device" ]
-    ] ],
+    [ "Adafruit_I2CDevice.h", "_adafruit___i2_c_device_8h.html", "_adafruit___i2_c_device_8h" ],
     [ "Adafruit_I2CRegister.h", "_adafruit___i2_c_register_8h.html", "_adafruit___i2_c_register_8h" ],
     [ "Adafruit_SPIDevice.cpp", "_adafruit___s_p_i_device_8cpp.html", null ],
     [ "Adafruit_SPIDevice.h", "_adafruit___s_p_i_device_8h.html", "_adafruit___s_p_i_device_8h" ]
