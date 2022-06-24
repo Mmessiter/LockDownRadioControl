bea8927b9d8796d36f53df8276c764786bc41b1f
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2022-06-24 16:01:28 +0100

    Update main.cpp

--- TransmitterCode/src/main.cpp
+++ TransmitterCode/src/main.cpp
@@ -4848,9 +4848,9 @@ void ExitMacrosView(){
 /*********************************************************************************************************************************/
 
 void  EndReverseView(){ // channel reverse flags are 16 individual BITs in ReversedChannelBITS
-        char fs[16][5]   = {"fs1","fs2","fs3","fs4","fs5","fs6","fs7","fs8","fs9","fs10","fs11","fs12","fs13","fs14","fs15","fs16"};
+        char fs[16][5]                 = {"fs1","fs2","fs3","fs4","fs5","fs6","fs7","fs8","fs9","fs10","fs11","fs12","fs13","fs14","fs15","fs16"};
         uint8_t i;    
-        char pSetupView[] = "page SetupView";
+        char pSetupView[]              = "page SetupView";
         char ProgressStart[]           = "vis Progress,1";
         char ProgressEnd[]             = "vis Progress,0";
         char Progress[]                = "Progress";
