992894dbe0206d22426caaa553278236d1b29b08
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2022-07-03 20:02:50 +0100

    Update RadioFunctions.h

--- TransmitterCode/src/Hardware/RadioFunctions.h
+++ TransmitterCode/src/Hardware/RadioFunctions.h
@@ -78,7 +78,7 @@
 #define PACEMAKER                   8    // MINIMUM ms between sent packets of data. These brief pauses allow the receiver to poll its i2c Sensor hub, and TX to ShowComms();
 #define RETRYCOUNT                  3    // auto retries inside nRF24L01
 #define RETRYWAIT                   1    // Wait between retries is RetryWait+1 * 250us. A failed packet therefore takes (RetryWait+1 * 250us) * RetryCount
-#define LOSTCONTACTCUTOFF           5    // How many packets to lose before reconnect triggers  (>=5) 5*8 = 40 ms (40 ms = RX timeout)
+#define LOSTCONTACTCUTOFF           2    // How many packets to lose before reconnect triggers  
 #define RECONNECT_CHANNELS_COUNT    3    // How many channels to try when reconnecting
 #define RECONNECT_CHANNELS_START    12   // Offset into channels' array
 #define RED_LED_ON_TIME             1000 // How many ms of no connection before RED led comes on
