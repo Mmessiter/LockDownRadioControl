19cae75906f68d323e04c2ea8c6679cc5b54b956
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2022-07-21 15:58:34 +0100

    Update main.cpp

--- Quadcopter/src/main.cpp
+++ Quadcopter/src/main.cpp
@@ -90,14 +90,16 @@ float maxRoll = 30.0;     //Max roll angle in degrees for angle mode (maximum 60
 float maxPitch = 30.0;    //Max pitch angle in degrees for angle mode (maximum 60 degrees), deg/sec for rate mode
 float maxYaw = 160.0;     //Max yaw rate in deg/sec
 // ***************************************************************************************************************************************************
-float Kp_roll_angle = 0.2;    //Roll P-gain - angle mode 
+//float Kp_roll_angle = 0.2;    //Roll P-gain - angle mode 
+float Kp_roll_angle = 0.3;    // MCM
 float Ki_roll_angle = 0.3;    //Roll I-gain - angle mode
 //float Kd_roll_angle = 0.05; //Roll D-gain - angle mode (if using controlANGLE2(), set to 0.0)
 float Kd_roll_angle   = 0.1;   // MCM
 // ***************************************************************************************************************************************************
 float B_loop_roll = 0.9;      //Roll damping term for controlANGLE2(), lower is more damping (must be between 0 to 1)
 // ***************************************************************************************************************************************************
-float Kp_pitch_angle = 0.2;   //Pitch P-gain - angle mode
+// float Kp_pitch_angle = 0.2;   //Pitch P-gain - angle mode
+float Kp_pitch_angle = 0.3;   //MCM
 float Ki_pitch_angle = 0.3;   //Pitch I-gain - angle mode
 //float Kd_pitch_angle = 0.05;  //Pitch D-gain - angle mode (if using controlANGLE2(), set to 0.0)
 float Kd_pitch_angle = 0.1;  // MCM
@@ -343,7 +345,7 @@ void loop() {
 
 void GetGains(){
 // Roll and Pitch  
-#define P_GAIN_DEFAULT 0.2                      // default roll and pitch P Gain is 0.2 - angle mode
+#define P_GAIN_DEFAULT 0.3                      // default roll and pitch P Gain WAS 0.2 - angle mode
 #define P_GAIN_MIN P_GAIN_DEFAULT / 2 
 #define P_GAIN_MAX P_GAIN_DEFAULT * 1.5  
 
