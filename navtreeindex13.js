a524ec9a9ffe9484a949ecd0e1ff850c8662575c
Mmessiter <Mmessiter@users.noreply.github.com>
2022-09-22 06:36:10 +0000

    deploy: 9a22c82323634690faba4fc3ba58f4669615f453

--- navtreeindex13.js
+++ navtreeindex13.js
@@ -1,6 +1,10 @@
 var NAVTREEINDEX13 =
 {
-"radio_8h.html#affa2dcc4341b8daecac77e6e0a354b2d":[10,0,2,1,0,1,18],
+"radio_8h.html#af56956a6b17dbdfc9fceedeb5a13cb71":[10,0,2,1,0,1,24],
+"radio_8h.html#af5ed7b5350f1500e976e56af6f24efb0":[10,0,2,1,0,1,73],
+"radio_8h.html#afabe4c9d9760f8606aa408474e3d626c":[10,0,2,1,0,1,51],
+"radio_8h.html#aff48e1463bf663c156032302d0161c98":[10,0,2,1,0,1,61],
+"radio_8h.html#affa2dcc4341b8daecac77e6e0a354b2d":[10,0,2,1,0,1,19],
 "radio_8h_source.html":[10,0,2,1,0,1],
 "struct_payload.html":[9,0,17],
 "struct_payload.html#a31d05bdbdf85fe184cc7f806d3391fa9":[9,0,17,2],
--- search/functions_12.js
+++ search/functions_12.js
@@ -10,9 +10,10 @@ var searchData=
   ['tinygpslocation_7',['TinyGPSLocation',['../struct_tiny_g_p_s_location.html#a9bc435af16c3c5224fcd4b5c40d0c70f',1,'TinyGPSLocation']]],
   ['tinygpsplus_8',['TinyGPSPlus',['../class_tiny_g_p_s_plus.html#a3d9173312d514abf50bd43efe6bf6af3',1,'TinyGPSPlus']]],
   ['tinygpstime_9',['TinyGPSTime',['../struct_tiny_g_p_s_time.html#abd6dd7a576fd42cd6980c92eec77ab1d',1,'TinyGPSTime']]],
-  ['transfer_10',['transfer',['../class_adafruit___s_p_i_device.html#a155f5efd84dbc240c987f4066265b840',1,'Adafruit_SPIDevice::transfer(uint8_t *buffer, size_t len)'],['../class_adafruit___s_p_i_device.html#ac60964386f37b2c498fc1b1540f61889',1,'Adafruit_SPIDevice::transfer(uint8_t send)']]],
+  ['transfer_10',['transfer',['../class_adafruit___s_p_i_device.html#ac60964386f37b2c498fc1b1540f61889',1,'Adafruit_SPIDevice::transfer(uint8_t send)'],['../class_adafruit___s_p_i_device.html#a155f5efd84dbc240c987f4066265b840',1,'Adafruit_SPIDevice::transfer(uint8_t *buffer, size_t len)']]],
   ['trynextchannel_11',['TryNextChannel',['../_receiver_code_2src_2main_8cpp.html#a1d10d2ca466b8014f874f16aaa39da91',1,'main.cpp']]],
   ['tryotherpipe_12',['TryOtherPipe',['../_radio_functions_8cpp.html#ad0e30213b21808d00110752679788a8c',1,'RadioFunctions.cpp']]],
-  ['trytoconnectnow_13',['TryToConnectNow',['../radio_8h.html#a1208ef26dd460e6f5f56582f4fbfcf94',1,'radio.h']]],
-  ['trytoreconnect_14',['TryToReconnect',['../_radio_functions_8cpp.html#ad0236abcfc887ad708d9504c4ab2a7c3',1,'RadioFunctions.cpp']]]
+  ['trytheothertransceiver_13',['TryTheOtherTransceiver',['../radio_8h.html#adfacfc7d075937cec51b7bcd08d323ca',1,'radio.h']]],
+  ['trytoconnectnow_14',['TryToConnectNow',['../radio_8h.html#a1208ef26dd460e6f5f56582f4fbfcf94',1,'radio.h']]],
+  ['trytoreconnect_15',['TryToReconnect',['../_radio_functions_8cpp.html#ad0236abcfc887ad708d9504c4ab2a7c3',1,'RadioFunctions.cpp']]]
 ];
