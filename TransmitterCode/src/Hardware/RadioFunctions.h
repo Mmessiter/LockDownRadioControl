1c5ca04688ca36d1e39a0a1cdc3ef920a4d578ed
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2022-10-16 12:38:12 +0100

    Update RadioFunctions.h

--- TransmitterCode/src/Hardware/RadioFunctions.h
+++ TransmitterCode/src/Hardware/RadioFunctions.h
@@ -37,7 +37,7 @@
 //    DEBUG OPTIONS (Uncomment any of these for that bit of debug info)     *
 //***************************************************************************
 
- #define DB_NEXTION        // Debug NEXTION
+// #define DB_NEXTION        // Debug NEXTION
 // #define DB_SD             // Debug SD card data
 // #define DB_FHSS           // Debug real time FHSS data
 // #define DB_SENSORS        // Debug Sensors
