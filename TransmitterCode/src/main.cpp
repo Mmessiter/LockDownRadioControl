6cbd0ec09d0c88b5e1abc0a83f63b99226525870
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2022-10-20 19:54:59 +0100

    Update main.cpp

--- TransmitterCode/src/main.cpp
+++ TransmitterCode/src/main.cpp
@@ -7859,7 +7859,7 @@ void CheckPowerOffButton()
 void FASTRUN ManageTransmitter(){
 
     uint32_t RightNow = millis();
-    if (((RightNow - TxPace) >= PACEMAKER - 4) && Connected) return; // *** If it's almost time to send data then do not start some other task which might take longer! ***
+    if (((RightNow - TxPace) >= PACEMAKER - 4) && BoundFlag) return; // *** If it's almost time to send data then do not start some other task which might take longer! ***
     if (RightNow - LastBankRead > 100) {                             // 10 times a second is plenty
         if (RightNow - LastTimeRead >= 1000) {                       // Once a second for these...
             ReadTime();                                              // Do the clock
