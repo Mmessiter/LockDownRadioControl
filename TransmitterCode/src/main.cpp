ca52b0884e233f66b54eefec0be01bac65282687
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2022-10-20 11:03:06 +0100

    Timing improvement ...
    ... // *** If it's almost time to send data then do not start some other task which might take longer! ***

--- TransmitterCode/src/main.cpp
+++ TransmitterCode/src/main.cpp
@@ -7857,7 +7857,10 @@ void CheckPowerOffButton()
 
 /************************************************************************************************************/
 void FASTRUN ManageTransmitter(){
-    if (millis() - LastBankRead > 100) {                        // 6.66666 times a second is plenty
+
+    uint32_t RightNow = millis();
+    if (((RightNow - TxPace) >= PACEMAKER - 3)) return;         // *** If it's almost time to send data then do not start some other task which might take longer! ***
+    if (RightNow - LastBankRead > 100) {                        // 10 times a second is plenty
         if (millis() - LastTimeRead >= 1000) {                  // Once a second for these...
             ReadTime();        // Do the clock
             GetStatistics();   // Do stats
