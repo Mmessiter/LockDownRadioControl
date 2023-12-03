a21276b1fd1c56bb980a8ac5feab0459038962f8
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2023-12-03 11:33:38 +0000

    Update transceiver.h

--- TransmitterCode/src/Hardware/transceiver.h
+++ TransmitterCode/src/Hardware/transceiver.h
@@ -282,11 +282,9 @@ FASTRUN void SendData()
         LoadPacketData();                                        // extra parameters appended to the data packet
 #ifdef USE_NEW_CHANNEL_MAPPING  
         uint8_t p = EncodeTheChangedChannels();
-        // if (p < 4) SendExtraParamemters(p);                    // If fewer than 4 channels have changed, send extra parameters in last four bytes
-         p = ((float) p * 1.5) + 3 ;                              // 1.5 is the compression ratio. 2 is the number of extra bytes for flags. 1 byte for luck!
-        // Look(p);
-         Compress(Datatosend.CompressedData, RawDataBuffer, UNCOMPRESSEDWORDS);                          // Compress 
-         if (Radio1.write(&Datatosend, p)) {SuccessfulPacket();} else {FailedPacket();}                  // Send the data packet complete with DataFlags 
+        p = ((float) p * 1.5) + 3;                               // 1.5 is the compression ratio. 2 is the number of extra bytes for flags - plus 1 byte because half a word is no good.
+        Compress(Datatosend.CompressedData, RawDataBuffer, UNCOMPRESSEDWORDS);                          // Compress 
+        if (Radio1.write(&Datatosend, p)) {SuccessfulPacket();} else {FailedPacket();}                  // Send the data packet complete with DataFlags 
     
 #else
         Compress(Datatosend.CompressedData, SendBuffer, UNCOMPRESSEDWORDS); // Compress 
