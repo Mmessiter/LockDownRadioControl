a0ed3eb1138068454ebcf15e9f259a2b187a552f
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2023-12-20 11:17:49 +0000

    Update common.h

--- ReceiverCode/src/utilities/common.h
+++ ReceiverCode/src/utilities/common.h
@@ -18,10 +18,10 @@
 
 #ifdef FHSS200MHZ
     #define HOPTIME           0     // (about 200 hops per second)
-    #define RECEIVE_TIMEOUT   14     //  was 14 
+    #define RECEIVE_TIMEOUT   14    //  was 14 
 #else
     #define HOPTIME           96     //  milliseconds between hops (about 10 hops per second)
-    #define RECEIVE_TIMEOUT   14 //16     //  was 14 
+    #define RECEIVE_TIMEOUT   14     //  was 14 still is 14     
 #endif
 
 // **************************************************************************
