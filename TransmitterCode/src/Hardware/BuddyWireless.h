06c292e53cf1e4f7f7caf968e487fdaa0eab706b
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2024-01-07 11:24:33 +0000

    Update BuddyWireless.h

--- TransmitterCode/src/Hardware/BuddyWireless.h
+++ TransmitterCode/src/Hardware/BuddyWireless.h
@@ -31,7 +31,7 @@ void LoadCorrectModel(uint64_t ModelID){                                        
         ModelNumber = SavedModelNumber;                                         //  Restore the current model number
         ReadOneModel(ModelNumber);                                              //  Restore the current model 
         ModelExistsAtBuddy = false;                                             //  Flag to indicate that the model isn't here so don't look again
-      //  Look("Model not found");                                                //  Tell the user
+    //  Look("Model not found");                                                //  Tell the user
         GotoFrontView();
     }
 }
@@ -40,10 +40,10 @@ void LoadCorrectModel(uint64_t ModelID){                                        
 //*************************************************************************************************************************
 
 void CheckModelMatchesMaster(uint64_t ModelID){
-    static uint32_t LastModelIDCheckTime = 0;                                           // The pupil checks the model ID once every second and loads the correct model if it !matches
-    if (millis()-LastModelIDCheckTime > 1000) {                                         // check the model ID once every second
+    static uint32_t LastModelIDCheckTime = 0;                                    // The pupil checks the model ID once every second and loads the correct model if it !matches
+    if (millis()-LastModelIDCheckTime > 1000) {                                  // check the model ID once every second
         LastModelIDCheckTime = millis();
-        if (ModelID!= ModelsMacUnionSaved.Val64) LoadCorrectModel(ModelID);             // if the model ID !matches, then load the correct model    
+        if (ModelID!= ModelsMacUnionSaved.Val64) LoadCorrectModel(ModelID);      // if the model ID !matches, then load the correct model    
     }
 }
 //*************************************************************************************************************************
@@ -79,8 +79,8 @@ void MasterDetected(bool Detected)
 
     if (Detected) {
         LostMasterCount = 0;
-        LastPassivePacketTime = millis();                   // reset the timer
-        if (MasterIsAlive != 1) {                           // MasterIsAlive = 1 means Master was already alive
+        LastPassivePacketTime = millis();                                   // reset the timer
+        if (MasterIsAlive != 1) {                                           // MasterIsAlive = 1 means Master was already alive
             SendText(wb, Mfound);
             SendCommand(YesVisible);
             MasterIsAlive = 1;
@@ -89,7 +89,7 @@ void MasterDetected(bool Detected)
     else {
         ++LostMasterCount;
         if (LostMasterCount > LOSTCONTACTTHRESHOLD) {
-            if (MasterIsAlive != 2) {                       // MasterIsAlive = 2 means Master was already dead
+            if (MasterIsAlive != 2) {                                        // MasterIsAlive = 2 means Master was already dead
                 SendText(wb, Mlost);
                 SendCommand(YesVisible); 
                 MasterIsAlive = 2;
@@ -139,30 +139,34 @@ void PupilDetected(bool Detected)
 // GetSlaveChannelValuesWireless() then uses the BuddyBuffer data to replace some or all of the Master's control data. 
 void GetPupilAck()                                    
 {
-    uint16_t AckSpecial[COMPRESSEDWORDS];                       // Ack payload will contain all the pupil's control data
-    if (Radio1.available()) {                                   // if a packet has arrived
-        Radio1.read(&AckSpecial, sizeof AckSpecial);            // read the packet
-        Decompress(BuddyBuffer, AckSpecial, UNCOMPRESSEDWORDS); // decompress the data into buddybuffer array
+    uint16_t AckSpecial[COMPRESSEDWORDS];                                   // Ack payload will contain all the pupil's control data
+    if (Radio1.available()) {                                               // if a packet has arrived
+        Radio1.read(&AckSpecial, sizeof AckSpecial);                        // read the packet
+        Decompress(BuddyBuffer, AckSpecial, UNCOMPRESSEDWORDS);             // decompress the data into buddybuffer array
     }
 }
 
 //*************************************************************************************************************************
-void ChangeTXTarget(uint8_t ch,uint64_t p, rf24_datarate_e speed)       // Swap between Buddy and Master
-{                                                                       // ch is the channel, p is the pipe address, speed is the data rate
-     Radio1.setDataRate(speed);                                         // Set the data rate
-     Radio1.openWritingPipe(p);                                         // Set the pipe address
+void ChangeTXTarget(uint8_t ch,uint64_t p, rf24_datarate_e speed)           // Swap between Buddy and Master
+{                                                                           // ch is the channel, p is the pipe address, speed is the data rate
+     Radio1.setDataRate(speed);                                             // Set the data rate
+     Radio1.openWritingPipe(p);                                             // Set the pipe address
      Radio1.stopListening();
      delayMicroseconds(STOPLISTENINGDELAY);
-     Radio1.setChannel(ch);                                             // Set the frequency channel
+     Radio1.setChannel(ch);                                                 // Set the frequency channel
      delayMicroseconds(STOPLISTENINGDELAY);
 }
 
 //*************************************************************************************************************************
-void SendSpecialPacket()                                                    // Here the MASTER sends to PUPIL tx. This is called about 100 times a second.
+void SendSpecialPacket()                                                    // Here the MASTER sends to PUPIL tx. This is called about 200 times a second.
 {                                                                           // Master Sends M or B to indicate whether Buddy is on or off, and the ID of the model which should be loaded.
+                                                                            // The Pupil's Ack Payload contains the pupil's control data (in BuddyBuffer[]) which is compressed and must be decompressed before use.
+                                                                            // This function is called from SendData() function which is called from the main loop.
+                                                                            // GetSlaveChannelValuesWireless() then uses the BuddyBuffer data to replace some or all of the Master's control data. 
+                                                                            // Because the datarate is 2 meg the exchange is very fast.
     static uint32_t LocalTimer      = 0;
     static bool     NeedToRecover   = false;
-    static uint8_t  NpOld           = 0;                                    // The old channel number
+    static uint8_t  ChannelSendLastTime = 0;                                // The old channel number
     static uint8_t  Index           = 82;                                   // The current channel number
   
      struct spd
@@ -182,24 +186,27 @@ void SendSpecialPacket()                                                    // H
     SpecialPacketData.ModelID =  ModelsMacUnionSaved.Val64;                 // Send the model ID so that pupil can check it
     SpecialPacketData.Command[0]                    = 'M';                  // Send M to indicate Master is ON
     if (BuddyON)       SpecialPacketData.Command[0] = 'B';                  // Send B to indicate Buddy is ON
-    NpOld = SpecialPacketData.Channel;                                           // Use the old channel number because Buddy hasn't yet hopped
+    ChannelSendLastTime = SpecialPacketData.Channel;                        // Use the old channel number because Buddy hasn't yet hopped
     --Index; if (Index < 1) Index = 82;                                     // use the same array but in reverse order
-    SpecialPacketData.Channel = FHSS_data::FHSS_Channels[Index];            // Set the channel number
+    SpecialPacketData.Channel = FHSS_data::FHSS_Channels[Index];            // Set the  new channel number for next time
     if (NeedToRecover) SpecialPacketData.Channel = QUIETCHANNEL;            // If contact lost, then use the recovery channel to recover
-    ChangeTXTarget(NpOld,TeensyMACAddPipe ^ ENCRYPT_KEY,faster);            // Set the TX target to the Buddy
+    ChangeTXTarget(ChannelSendLastTime,TeensyMACAddPipe ^ ENCRYPT_KEY,faster); // Set the TX target to the Buddy
     if (Radio1.write(&SpecialPacketData, sizeof SpecialPacketData)) {       // Send the packet
         GetPupilAck();                                                      // Get ack from pupil WITH HIS CONTROL DATA!!
         PupilDetected(true);                                                // Pupil is alive
-        NeedToRecover = false;
+        NeedToRecover = false;                                              // No need to recover             
     } else {
         PupilDetected(false);                                               // Pupil is dead
-        NeedToRecover = true;
+        NeedToRecover = true;                                               // Need to recover                 
     }
-    ChangeTXTarget(CurrentChannel,TeensyMACAddPipe,slower);
+    ChangeTXTarget(CurrentChannel,TeensyMACAddPipe,slower);                 // Set the TX target back to the receiver in model.
 }
 //*************************************************************************************************************************
-void GetSpecialPacket()                                                                 // here the PUPIL tx gets from MASTER tx. This function is called from main loop about 100 times a second.
-{                                                                                       // Master tells Pupil whether buddy is on or off, and the ID of the model which should be loaded.
+void GetSpecialPacket()                                                     // Here the PUPIL tx gets a packet from MASTER tx. This function is called from main loop very fast.
+{                                                                           // Master tells Pupil whether buddy is on or off, and the ID of the model which should be loaded.
+                                                                            // The Pupil's Ack Payload contains the pupil's control data (in BuddyBuffer[]) which is compressed and must be decompressed before use.
+                                                                            // GetSlaveChannelValuesWireless() in Master then uses the BuddyBuffer data to replace some or all of the Master's control data. 
+                                                                            // Because the datarate is 2 meg the exchange is very fast. 
     static bool MasterIsInControl = true; 
     struct spd {
         char        Command[2];
