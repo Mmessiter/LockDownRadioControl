03ffb99a65165fcf668b7f95c22f6c710ea5a67c
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2025-03-20 22:06:15 +0000

    update

--- TransmitterCode/src/Hardware/MenuOptions.h
+++ TransmitterCode/src/Hardware/MenuOptions.h
@@ -1110,6 +1110,7 @@ void BuddyChViewEnd()
     char ProgressStart[] = "vis Progress,1";
     char fs[16][5] = {"fs1", "fs2", "fs3", "fs4", "fs5", "fs6", "fs7", "fs8", "fs9", "fs10", "fs11", "fs12", "fs13", "fs14", "fs15", "fs16"};
     char mSwitch[] = "mSwitch";
+    char mSwi[] = "mSwi"; // not sure why we need this but the full length string is not always found
     bool Altered = false;
     char chgs[512];
     char change[] = "change";
@@ -1120,9 +1121,12 @@ void BuddyChViewEnd()
         chgs[i + 1] = 0;
     }
     SendCommand(ProgressStart);
-    if (InStrng(mSwitch, chgs))
+    if (InStrng(mSwi, chgs))
     {
-        BuddyHasAllSwitches = GetValue(mSwitch);
+        if (GetValue(mSwitch))
+            BuddyHasAllSwitches = true;
+        else
+            BuddyHasAllSwitches = false;
         Altered = true;
     }
     for (int i = 0; i < 16; ++i)
