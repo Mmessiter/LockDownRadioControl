09fb23c576eb070aea34304135a03958da4b624c
Malcolm Messiter <66127058+Mmessiter@users.noreply.github.com>
2025-08-14 11:29:44 +0100

    Update 1Definitions.h

--- ReceiverCode/src/utilities/1Definitions.h
+++ ReceiverCode/src/utilities/1Definitions.h
@@ -22,15 +22,17 @@
 //  #define DB_FAILSAFE
 //  #define DB_RXTIMERS
 
-// >>>>>>>>>>>>>>>>>******* DON'T FORGET TO SET THESE LOT !!! ******* <<<<<<<<<<<<<<<<<<<<< **** <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-//
+// >>>>>>>>>>>>>>>>>******* DON'T FORGET TO SET THIS LOT !!! ******* <<<<<<<<<<<<<<<<<<<<< **** <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+ //#define USE_NEXUS // if DEFINED then USE_PWM must be UNDEFINED ( = commented out) and USE_SBUS must be DEFINED ( = uncommented) to use the Nexus transceiver
+
 #define SECOND_TRANSCEIVER // must be UNDEFINED ( = commented out) if using ONE transceiver but DEFINED if using TWO transceivers!
-//#define USE_11PWM_OUTPUTS  // must be UNDEFINED ( = commented out) if NOT using all 11 PWM outputs (i.e. older rxs with only 8 outputs) but DEFINED if using all 11 PWM outputs!
-
- #define USE_NEXUS // if DEFINED then USE_PWM must be UNDEFINED ( = commented out) and USE_SBUS must be DEFINED ( = uncommented) to use the Nexus transceiver
-// #define USE_PWM
-
+#define USE_11PWM_OUTPUTS  // must be UNDEFINED ( = commented out) if NOT using all 11 PWM outputs (i.e. older rxs with only 8 outputs) but DEFINED if using all 11 PWM outputs!
 #define USE_SBUS
+
+#ifndef USE_NEXUS
+// #define USE_PWM
+#endif // USE_NEXUS
 
 // **************************************************************************
 #define NEXUS_SERIAL_TELEMETRY Serial1
